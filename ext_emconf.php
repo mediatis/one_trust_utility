5767aebfa271ac548040e2ee17b93a7ffb15ccca
s-ude <ude@mediatis.de>
2024-10-25 14:53:48 +0200

    [TASK] TYPO3 12 Compatibility (#3)

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => '',
     'author_email' => 'info@mediatis.de',
     'state' => 'stable',
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
